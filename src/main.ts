2b0854c4bb5092da2905a78bc0e77354143a1ef1
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2023-02-21 13:51:50 +0100

    fix: linting

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import { createApi } from 'unsplash-js';
 type State = {
   searchHistory: string[],
   currentSearch: string
-}
+};
 
 let state: State = {
   searchHistory: [],
-  currentSearch: 'cat'
-}
+  currentSearch: 'cat',
+};
+
+const update = (newState : State) => {
+  state = { ...state, ...newState }; // patch state, overwrite old data with new properties
+  window.dispatchEvent(new Event('statechange'));
+};
 
 const unsplash = createApi({
   accessKey: '5vcBJ_NanyB5xxCKoPCfkrIURjCnfB9TQY2vKGeABXA',
@@ -19,46 +24,39 @@ htmlSearch.addEventListener('click', () => {
   const searchInput = document.getElementById('searchText') as HTMLInputElement;
   const textValue = searchInput.value;
   state.searchHistory.push(textValue);
-  update({ searchHistory: state.searchHistory, currentSearch: textValue })
-  console.log(state.searchHistory)
+  update({ searchHistory: state.searchHistory, currentSearch: textValue });
 });
 
 const fetchingPhotos = () => {
   unsplash.search
-    .getPhotos({ query: state.currentSearch, orientation: 'landscape', perPage: 9})
+    .getPhotos({ query: state.currentSearch, orientation: 'landscape', perPage: 9 })
     .then(result => {
-      console.log(result);
-      document.querySelector<HTMLDivElement>('#app')!.innerHTML='';
+      document.querySelector<HTMLDivElement>('#app')!.innerHTML = '';
       result.response?.results.map(item => {
         const imgElement = document.createElement('img');
         imgElement.setAttribute('src', item.urls.small);
-        imgElement.setAttribute('alt', item.alt_description || "no information for this image")
+        imgElement.setAttribute('alt', item.alt_description || 'no information for this image');
         document.querySelector<HTMLDivElement>('#app')!.append(imgElement);
       });
     })
     .catch(() => {
-      console.log('something went wrong!');
     });
 };
-const update = (newState : State) => {
-  state = { ...state, ...newState }; // patch state, overwrite old data with new properties 
-  window.dispatchEvent(new Event('statechange'));
-  };
+
+const searchSuggestion = () => {
+  const suggestionsElement = document.getElementById('searchText');
+  suggestionsElement?.addEventListener('focus', () => {
+    const ulElement = document.getElementById('suggestionList') as HTMLUListElement;
+    ulElement.innerHTML = '';
+    state.searchHistory.map(item => {
+      const liElement = document.createElement('li');
+      liElement.innerHTML = item;
+      ulElement.append(liElement);
+    });
+  });
+};
+searchSuggestion();
 
 window.addEventListener('statechange', () => {
   fetchingPhotos();
-})
-
-const searchSuggestion = () => {
-  const suggestionsElement = document.getElementById('searchText')
-  suggestionsElement?.addEventListener('focus', () => {
-    const ulElement = document.getElementById('suggestionList') as HTMLUListElement
-    ulElement.innerHTML = ''
-    state.searchHistory.map(item => {
-      const liElement = document.createElement('li')
-      liElement.innerHTML = item;
-      ulElement.append(liElement);
-    })
-  })
-}
-searchSuggestion();
+});
