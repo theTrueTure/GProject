f058b159741f4a68338dc4c11c2b528b6fb0a984
senne-hartland <119686535+senne-hartland@users.noreply.github.com>
2023-02-21 11:49:33 +0100

    feature: added search function
    search function state history working. Need to fix enter search.

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,37 @@
 import { createApi } from 'unsplash-js';
 
+type State = {
+  searchHistory: string[],
+  currentSearch: string
+}
+
+let state: State = {
+  searchHistory: [],
+  currentSearch: 'cat'
+}
+
 const unsplash = createApi({
   accessKey: '5vcBJ_NanyB5xxCKoPCfkrIURjCnfB9TQY2vKGeABXA',
 });
 
-const userInput = (cb: Function) => {
-  const htmlSearch = document.getElementById('searchButton') as HTMLButtonElement;
-  htmlSearch.addEventListener('click', () => {
-    const searchInput = document.getElementById('searchText') as HTMLInputElement;
-    const textValue = searchInput.value;
-    const clientSearch = document.getElementById('demo') as HTMLParagraphElement;
-    clientSearch.innerHTML = textValue;
-    cb(textValue);
-  });
-};
+const htmlSearch = document.getElementById('searchButton') as HTMLButtonElement;
+htmlSearch.addEventListener('click', () => {
+  const searchInput = document.getElementById('searchText') as HTMLInputElement;
+  const textValue = searchInput.value;
+  const clientSearch = document.getElementById('demo') as HTMLParagraphElement;
+  clientSearch.innerHTML = textValue;
+  state.searchHistory.push(textValue);
+  update({ searchHistory: state.searchHistory, currentSearch: textValue })
+  console.log(state.searchHistory)
+  // state.searchHistory.push(textValue);
+});
 
-const fetchingPhotos = (userSearch: string) => {
+const fetchingPhotos = () => {
   unsplash.search
-    .getPhotos({ query: userSearch, orientation: 'landscape' })
+    .getPhotos({ query: state.currentSearch, orientation: 'landscape', perPage: 9})
     .then(result => {
       console.log(result);
+      document.querySelector<HTMLDivElement>('#app')!.innerHTML='';
       result.response?.results.map(item => {
         const imgElement = document.createElement('img');
         imgElement.setAttribute('src', item.urls.small);
@@ -31,5 +43,12 @@ const fetchingPhotos = (userSearch: string) => {
       console.log('something went wrong!');
     });
 };
+const update = (newState : State) => {
+  state = { ...state, ...newState }; // patch state, overwrite old data with new properties 
+  window.dispatchEvent(new Event('statechange'));
+  };
 
-userInput(fetchingPhotos);
+window.addEventListener('statechange', () => {
+  fetchingPhotos();
+})
+
