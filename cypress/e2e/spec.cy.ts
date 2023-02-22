22e621f6b7d79518bf80f3cf41b06e0e014d8c8b
Mariana <118630670+mariana-molina@users.noreply.github.com>
2023-02-22 10:40:21 +0100

    feat: added workflow  yml

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,19 +1,12 @@
 describe('template spec', () => {
-  beforeEach(() => {
-    cy.visit('http://127.0.0.1:5173/');
-  });
-  it('should go to site', () => { });
-  it('should be able to search', () => {
-    cy.get('#searchText').type('dogs');
-  });
-  it('search button should work', () => {
-    cy.get('#searchButton').click();
-  });
-  it('should count 10 images', () => {
-    cy.get('#searchText').type('dogs');
-    cy.get('#searchButton').click();
-    cy.get('#app').find('img').should('have.length', 10);
-  });
+	beforeEach(() => {
+		cy.visit('http://127.0.0.1:5173/');
+	});
+	it('should count 10 images', () => {
+		cy.get('#searchText').type('dogs');
+		cy.get('#searchButton').click();
+		cy.get('#app').find('img').should('have.length', 10);
+	});
 });
 
 export default {};
--- src/main.ts
+++ src/main.ts
@@ -1,72 +1,72 @@
 import { createApi } from 'unsplash-js';
 
 type State = {
-	searchHistory: string[];
-	currentSearch: string;
+  searchHistory: string[];
+  currentSearch: string;
 };
 
 let state: State = {
-	searchHistory: ['dogs', 'babies'],
-	currentSearch: 'cat',
+  searchHistory: ['dogs', 'babies'],
+  currentSearch: 'cat',
 };
 
 const update = (newState: State) => {
-	state = { ...state, ...newState };
-	window.dispatchEvent(new Event('statechange'));
+  state = { ...state, ...newState };
+  window.dispatchEvent(new Event('statechange'));
 };
 
 const unsplash = createApi({
-	accessKey: 'O3jf0Le_tBem9sMo0Y2Nlis9VKNAVqEsTDPUzZWetb4',
+  accessKey: 'O3jf0Le_tBem9sMo0Y2Nlis9VKNAVqEsTDPUzZWetb4',
 });
 
 const htmlSearch = document.getElementById('searchButton') as HTMLButtonElement;
 htmlSearch.addEventListener('click', () => {
-	const searchInput = document.getElementById('searchText') as HTMLInputElement;
-	const textValue = searchInput.value;
-	state.searchHistory.push(textValue);
-	update({ searchHistory: state.searchHistory, currentSearch: textValue });
+  const searchInput = document.getElementById('searchText') as HTMLInputElement;
+  const textValue = searchInput.value;
+  state.searchHistory.push(textValue);
+  update({ searchHistory: state.searchHistory, currentSearch: textValue });
 });
 
 export const fetchingPhotos = async (keyword: string) => {
-	const unsplashObj = await unsplash.search
-		.getPhotos({ query: keyword, orientation: 'landscape' })
-		.then(result => result);
-	return unsplashObj;
+  const unsplashObj = await unsplash.search
+    .getPhotos({ query: keyword, orientation: 'landscape' })
+    .then(result => result);
+  return unsplashObj;
 };
 
 const addtoDom = async () => {
-	const unsplashObj = await fetchingPhotos(state.currentSearch);
-	const imgsElement = document.querySelector('#app') as HTMLDivElement;
-	imgsElement.innerHTML = '';
-	unsplashObj.response?.results.forEach(item => {
-		const imgElement = document.createElement('img');
-		imgElement.setAttribute('src', item.urls.small);
-		imgElement.setAttribute(
-			'alt',
-			item.alt_description || 'no information for this image'
-		);
-		document.querySelector<HTMLDivElement>('#app')!.append(imgElement);
-	});
+  const unsplashObj = await fetchingPhotos(state.currentSearch);
+  const imgsElement = document.querySelector('#app') as HTMLDivElement;
+  imgsElement.innerHTML = '';
+  unsplashObj.response?.results.forEach(item => {
+    const imgElement = document.createElement('img');
+    imgElement.setAttribute('src', item.urls.small);
+    imgElement.setAttribute(
+      'alt',
+      item.alt_description || 'no information for this image',
+    );
+    document.querySelector<HTMLDivElement>('#app')!.append(imgElement);
+  });
 };
 
 const searchSuggestion = () => {
-	const suggestionsElement = document.getElementById('searchText');
-	suggestionsElement?.addEventListener('focus', () => {
-		const ulElement = document.getElementById(
-			'suggestionList'
-		) as HTMLUListElement;
-		ulElement.innerHTML = '';
-		state.searchHistory.forEach(item => {
-			const liElement = document.createElement('li');
-			liElement.innerHTML = item;
-			ulElement.append(liElement);
-		});
-	});
+  const suggestionsElement = document.getElementById('searchText');
+  suggestionsElement?.addEventListener('focus', () => {
+    const ulElement = document.getElementById(
+      'suggestionList',
+    ) as HTMLUListElement;
+    ulElement.innerHTML = '';
+    state.searchHistory.forEach(item => {
+      const liElement = document.createElement('li');
+      liElement.innerHTML = item;
+      ulElement.append(liElement);
+    });
+  });
 };
 searchSuggestion();
 
 window.addEventListener('statechange', () => {
-	addtoDom();
+  addtoDom();
 });
 
 export default {};
